a56004a37f4fcfd106c8ffc89321ceeb3947b821
Traktuner <78533119+traktuner@users.noreply.github.com>
2024-03-27 09:12:21 +0100

    fix for "brew command not found" on arm64

--- utils/02-homebrew.sh
+++ utils/02-homebrew.sh
@@ -2,11 +2,16 @@
 
 echo "=> Homebrew"
 
+arch=$(uname -m)
+
 if [[ ! -f $(which brew) ]]
 then
   print_info "Installing..."
 
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if [[ $arch == "arm64" ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
   brew bundle --file="$ROOT_DIR/core/Brewfile"
   brew cleanup && brew upgrade && brew update && brew doctor
 
