d7b512c4c4b4c3c4719f829a07904e15284e32ed
Traktuner <78533119+traktuner@users.noreply.github.com>
2024-08-26 16:04:17 +0200

    correct mackup path

--- utils/05-mackup.sh
+++ utils/05-mackup.sh
@@ -4,5 +4,5 @@ echo "=> Backup restore"
 
 CLOUD_DIR=$(echo "$HOME/Library/Mobile Documents/com~apple~CloudDocs")
 
-cp "$CLOUD_DIR/.config/.mackup.cfg" "$HOME"
+cp "$CLOUD_DIR/.config/mackup/.mackup.cfg" "$HOME"
 mackup restore
