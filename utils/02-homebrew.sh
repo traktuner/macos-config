da9abc6aac01b7176ddda66aa2c10017604c884a
Traktuner <traktuner@pm.me>
2025-05-13 07:08:29 +0200

    init and persist brew

--- utils/02-homebrew.sh
+++ utils/02-homebrew.sh
@@ -22,23 +22,19 @@ else
 fi
 
 # 2) Init and persist shellenv
-- eval "$(/opt/homebrew/bin/brew shellenv)"
--
-- BREW_ENV_CMD='eval "$(/opt/homebrew/bin/brew shellenv)"'
-- for PROFILE in "$HOME/.zprofile" "$HOME/.zshrc"; do
-+ # 2a) For this running script
-+ eval "$(/opt/homebrew/bin/brew shellenv)"
-+
-+ # 2b) For all future zsh sessions, inject into both login and non-login shells
-+ BREW_SHELLENV='eval "$(/opt/homebrew/bin/brew shellenv)"'
-+ for PROFILE in "$HOME/.zprofile" "$HOME/.zshenv"; do
-     [[ -f $PROFILE ]] || touch "$PROFILE"
-     if ! grep -Fxq "$BREW_SHELLENV" "$PROFILE"; then
-       print_info "Appending Homebrew shellenv to $PROFILE"
-       printf '\n# Load Homebrew\n%s\n' "$BREW_SHELLENV" >>"$PROFILE"
-       print_success "Appended brew shellenv to $PROFILE"
-     fi
-   done
+# a) in this script:
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# b) for all future zsh sessions:
+BREW_SHELLENV='eval "$(/opt/homebrew/bin/brew shellenv)"'
+for PROFILE in "$HOME/.zprofile" "$HOME/.zshenv"; do
+  [[ -f $PROFILE ]] || touch "$PROFILE"
+  if ! grep -Fxq "$BREW_SHELLENV" "$PROFILE"; then
+    print_info "Appending Homebrew shellenv to $PROFILE"
+    printf '\n# Load Homebrew\n%s\n' "$BREW_SHELLENV" >>"$PROFILE"
+    print_success "Appended brew shellenv to $PROFILE"
+  fi
+done
 
 
 # 3) Taps
